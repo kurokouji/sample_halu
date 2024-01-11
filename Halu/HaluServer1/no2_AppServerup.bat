979f42ee50bce75e805e8c0a4dd21e666d8acce9
moe-kato <m.kato@tills.co.jp>
2024-01-12 01:03:41 +0900

    add

---
+++ Halu/HaluServer1/no2_AppServerup.bat
@@ -0,0 +1,9 @@
+@echo on
+
+rem メインファルダーに移動する
+cd ./Halu/System/Server/Libraries/halumain/
+
+rem Appサーバを起動する
+python appserver.py
+
+exit
--- no1_WebServerup.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-rem Haluファルダーに移動する
-cd ./Halu/
-
-rem Webサーバ＆スターレットを起動する
-uvicorn halu:app
-
-
